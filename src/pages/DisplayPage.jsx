3e4f1e46c8a70d4207bf73126bb6f8bb0ec6eb7d
billie89-33 <bowvorn@gmail.com>
2026-04-28 11:26:27 +0700

    add file .css

--- src/pages/DisplayPage.jsx
+++ src/pages/DisplayPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import "./DisplayPage.css"
 
 const DisplayPage = () => {
   return (
--- src/pages/InputPage.jsx
+++ src/pages/InputPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import "./InputPage.css";
 
 const InputPage = () => {
   return (
